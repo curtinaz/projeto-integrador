ac61057ebd54b47a6b3053c018585ee82bc2728d
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2020-12-13 17:00:20 -0300

    .out retirado

--- scripts.js
+++ scripts.js
@@ -1,18 +1,6 @@
-window.onload = init;
 
-function init() {
-  document.getElementById("myDiv").onclick = addMyClass;
-}
-
-function addMyClass() {
-  var classString = this.className; // returns the string of all the classes for myDiv
-  var newClass = classString.concat(" main__section"); // Adds the class "main__section" to the string (notice the leading space)
-  this.className = newClass; // sets className to the new string
-}
-
-
-inicio = document.querySelector('#inicio')
-outro = document.querySelector('#outro')
+const inicio = document.querySelector('#inicio')
+const outro = document.querySelector('#outro')
 
 function clickinicio() {
     inicio = document.querySelector('#inicio')
@@ -27,7 +15,7 @@ function clickoutro() {
     inicio = document.querySelector('#inicio')
     outro = document.querySelector('#outro')
     inicio.style = "display:none; transition:500ms";
-    outro.addClass("in")
-    // outro.style = "display:flex; transition:500ms";
+
+    outro.addin()
 
 }
