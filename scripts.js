c8b9d0a8a54109ce9036d8be5d0d7a56a302f650
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2020-12-13 19:13:10 -0300

    added login

--- scripts.js
+++ scripts.js
@@ -5,21 +5,31 @@
 function clickinicio() {
     inicio = document.querySelector('#inicio')
     videos = document.querySelector('#videos')
+    login = document.querySelector('#login')
 
     videos.style = "display:none; transition:500ms";
 
     inicio.style = "display:flex; transition:500ms";
-
 }
 
 function clickvideos() {
     inicio = document.querySelector('#inicio')
     videos = document.querySelector('#videos')
+    login = document.querySelector('#login')
 
     inicio.style = "display:none; transition:500ms";
 
     videos.style = "display:flex; transition:500ms";
+}
 
+function clickentrar() {
+    inicio = document.querySelector('#inicio')
+    videos = document.querySelector('#videos')
+    login = document.querySelector('#login')
+
+    inicio.style = "display:none; transition:500ms";
+
+    videos.style = "display:flex; transition:500ms";
 }
 
 // Não arrastar o logo
