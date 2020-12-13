2ce55a590c01cf3b19342d9913a9302fc94e5c43
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2020-12-13 17:24:18 -0300

    added logo

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
 
-const inicio = document.querySelector('#inicio')
-const outro = document.querySelector('#outro')
+// const inicio = document.querySelector('#inicio')
+// const outro = document.querySelector('#outro')
 
 function clickinicio() {
     inicio = document.querySelector('#inicio')
     outro = document.querySelector('#outro')
+
     outro.style = "display:none; transition:500ms";
 
     inicio.style = "display:flex; transition:500ms";
@@ -14,8 +15,9 @@ function clickinicio() {
 function clickoutro() {
     inicio = document.querySelector('#inicio')
     outro = document.querySelector('#outro')
+
     inicio.style = "display:none; transition:500ms";
 
-    outro.addin()
+    outro.style = "display:flex; transition:500ms";
 
 }
