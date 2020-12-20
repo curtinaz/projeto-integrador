aa43154c1b6f6531e9280762fafa71019e1448df
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2020-12-20 01:47:25 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -89,6 +89,9 @@ function clickvid1() {
     create = document.querySelector('#create')
 
     vid1 = document.querySelector('#vid1')
+    vid2 = document.querySelector('#vid2')
+    vid3 = document.querySelector('#vid3')
+    vid4 = document.querySelector('#vid4')
 
     inicio.style = "display:none; transition:500ms";
     videos.style = "display:none; transition:500ms";
