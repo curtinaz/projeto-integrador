fdaebb8c5db692ec1a515fc0a4c53a5f307be24b
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2020-12-20 01:47:51 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,7 +11,11 @@ function clickinicio() {
     videos = document.querySelector('#videos')
     login = document.querySelector('#login')
     create = document.querySelector('#create')
+
     vid1 = document.querySelector('#vid1')
+    vid2 = document.querySelector('#vid2')
+    vid3 = document.querySelector('#vid3')
+    vid4 = document.querySelector('#vid4')
     
     videos.style = "display:none; transition:500ms";
     login.style = "display:none; transition:500ms";
